10ad83f8c0afd3a86ef31116fd4dad769c09f8f6
Knooowname <91739752+Knooowname@users.noreply.github.com>
2026-03-03 16:45:31 +0500

    03.03.2026_16:45
    03.03.2026_16:45

--- source/certificate_tracker_frontend/src/widgets/settings-users-and-categoryes-info/ui/SettingsUserItem.tsx
+++ source/certificate_tracker_frontend/src/widgets/settings-users-and-categoryes-info/ui/SettingsUserItem.tsx
@@ -1,22 +1,42 @@
+import { User } from '@/src/entities'
 import { FormBtn } from '@/src/shared'
 import { FC } from 'react'
 
 interface SettingsUserItemProps {
-	label: string
-	value: string
+	user: User
 }
 
-export const SettingsUserItem: FC<SettingsUserItemProps> = ({
-	label,
-	value
-}) => {
+export const SettingsUserItem: FC<SettingsUserItemProps> = ({ user }) => {
 	return (
-		<div className="flex justify-between gap-[4px] px-[8px] py-[16px] border-b-1 border-gray-200">
-			<div>
-				<span className="text-[12px] text-gray-400">{label}</span>
-				<p className="text-[16px] text-[#202020] font-medium">
-					{value.at(0)?.toUpperCase() + value.slice(1).toLowerCase()}
-				</p>
+		<div className="flex flex-col items-start gap-[12px] px-[8px] py-[16px] border-b-1 border-gray-200">
+			<div className="flex items-center gap-[8px]">
+				<div>
+					<span className="text-[12px] text-gray-400">Имя Фамилия</span>
+					<div className="flex items-center gap-[4px]">
+						<p className="text-[16px] text-[#202020] font-medium">
+							{user.firstname.at(0)?.toUpperCase() +
+								user.firstname.slice(1).toLowerCase()}
+						</p>
+						<p className="text-[16px] text-[#202020] font-medium">
+							{user.lastname.at(0)?.toUpperCase() +
+								user.lastname.slice(1).toLowerCase()}
+						</p>
+					</div>
+				</div>
+				<div>
+					<span className="text-[12px] text-gray-400">E-mail</span>
+					<div className="flex items-center gap-[4px]">
+						<p className="text-[16px] text-[#202020] font-medium">
+							{user.firstname.at(0)?.toUpperCase() +
+								user.firstname.slice(1).toLowerCase()}
+						</p>
+						<p className="text-[16px] text-[#202020] font-medium">
+							{user.lastname.at(0)?.toUpperCase() +
+								user.lastname.slice(1).toLowerCase()}
+						</p>
+					</div>
+				</div>
+				<div></div>
 			</div>
 			<FormBtn text="Редактировать" />
 		</div>
